32df7a8f1a8865c07119a26f0c1e34dc82292949
Karl Roberts <karl.roberts@netsells.co.uk>
2016-10-02 17:42:48 +0100

    start documenting the core classes in use

--- src/Support/KarowayProperty.php
+++ src/Support/KarowayProperty.php
@@ -3,18 +3,51 @@
 namespace Netsells\Karoway\Support;
 
 
+use App\Karoway\Properties\Property;
+
 abstract class KarowayProperty
 {
+    /**
+     * The value from the database.
+     *
+     * @var string
+     */
     public $value;
 
+    /**
+     * The key from the database.
+     *
+     * @var string
+     */
     public $valueKey;
 
+    /**
+     * The property model.
+     *
+     * @var Property
+     */
     protected $property;
+
+    /**
+     * The classes to be bind to the element.
+     *
+     * @var string
+     */
 
     protected $classes = '';
 
+    /**
+     * The container element, the element that will encapsulate our value.
+     *
+     * @var string
+     */
+
     protected $wrapper = 'span';
 
+    /**
+     * assign the model and get custom configuration.
+     * @param $property
+     */
     public function __construct($property)
     {
         $this->property = $property;
@@ -22,6 +55,12 @@ abstract class KarowayProperty
         $this->value = $property->{$this->valueKey} ?? '';
     }
 
+    /**
+     * This sets the wrapper.
+     *
+     * @param $tag
+     * @return $this
+     */
     public function wrap($tag)
     {
         $this->wrapper = $tag;
@@ -29,6 +68,12 @@ abstract class KarowayProperty
         return $this;
     }
 
+    /**
+     * This allows the user to apply classes to the element that will contain the value.
+     *
+     * @param $classes
+     * @return $this
+     */
     public function classes($classes)
     {
         $this->classes .= $classes;
@@ -36,12 +81,22 @@ abstract class KarowayProperty
         return $this;
     }
 
+    /**
+     * See if we have a value, if we don't default to an empty string.
+     *
+     * @return string
+     */
     protected function getValue() : string
     {
         return $this->value ?? '';
     }
 
-    public function getClassAttribute()
+    /**
+     * Get the class attribute with the user specified classes.
+     *
+     * @return string
+     */
+    protected function getClassAttribute()
     {
         if ($this->classes) {
             return "class=\"{$this->classes}\"";
@@ -50,6 +105,11 @@ abstract class KarowayProperty
         return '';
     }
 
+    /**
+     * If we want a wrapper this will construct it with the specified classes.
+     *
+     * @return string
+     */
     public function getWrapper()
     {
         if ($this->wrapper) {
@@ -57,11 +117,21 @@ abstract class KarowayProperty
         }
     }
 
+    /**
+     * Closes out the element.
+     *
+     * @return string
+     */
     public function getClosingWrapper()
     {
         return "</{$this->wrapper}>";
     }
 
+    /**
+     * Whips the element to get along with the value inside it.
+     *
+     * @return string
+     */
     public function __toString()
     {
         return "{$this->getWrapper()}{$this->getValue()}{$this->getClosingWrapper()}";
